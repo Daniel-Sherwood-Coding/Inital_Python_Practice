d006f2d7e33bfe1612bedfcc1fff347a039a7d74
Daniel-Sherwood-Coding <danielsherwood2014@gmail.com>
2024-08-14 22:10:55 +0100

    Finished 1st method of inverting binary tree

--- Binary Tree.py
+++ Binary Tree.py
@@ -1,3 +1,4 @@
+# method 1: inversion through recursion
 class node:
     def __init__(self,data):
         self.left = None
@@ -9,11 +10,19 @@ class node:
         print(self.data, end= ' '),
         if self.right:
             self.right.printTree()
+    def invertTree(self, root):
+        if root == None:
+            return
+        root.left, root.right = self.invertTree(root.right),self.invertTree(root.left)
+        return root
 
 tree = node(1)
 tree.left = node(2)
 tree.right = node(3)
 tree.left.left = node(4)
 tree.left.right = node(5)
-
 tree.printTree()
+invertedTree = tree.invertTree(root= tree)
+invertedTree.printTree()
+
+# method 2: inversion through 
